dbf299b8361d54a4287879b7ae717cd301502f11
Yusuke Kambara <yusukekambara@gmail.com>
2020-02-18 16:56:02 +0900

    Added the test moudule for commands.analyze

--- tests/commands/test_analyze.py
+++ tests/commands/test_analyze.py
@@ -1,7 +1,7 @@
 import unittest
 import io
 import pandas as pd
-from commands import analyze as command_analyze
+from commands import analyze as commands_analyze
 
 class TestCommandsAnalyze(unittest.TestCase):
     """Test class for testing the analyze command module
@@ -14,4 +14,22 @@ class TestCommandsAnalyze(unittest.TestCase):
     def setUpClass(cls):
         print("*" * 80 + "\nStart to test [commands.analyze] module\n" + "*" * 80)
 
+    def test_analyze_normal_case(self):
+        """Testing to be able to get the analyzed resutl 
+        """
+        test_data = """x,y,z
+        12,22.45,777
+        test,2019-09-17,0098
+        """
+        result = commands_analyze.analyze(io.StringIO(test_data))
+        assert all(["most_common_type" in result[key] for key in result.keys()])
+        assert all(["included_types" in result[key] for key in result.keys()])
+        assert all(["loss_rate" in result[key] for key in result.keys()])
+        assert all(["all_items" in result[key] for key in result.keys()])
+        assert all(["unique_items" in result[key] for key in result.keys()])
 
+    def test_analyze_irregular_case(self):
+        """Testing to be able to get the analyzed resutl 
+        """
+        test_data = """,,"""
+        assert commands_analyze.analyze(io.StringIO(test_data)) == {}
