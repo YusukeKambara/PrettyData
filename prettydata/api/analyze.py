8d4ea57a54b98c07a9369f485a5b79d261159351
Yusuke Kambara <yusukekambara@gmail.com>
2020-02-19 16:19:09 +0900

    Incrase the performance to convert to type value

--- prettydata/api/analyze.py
+++ prettydata/api/analyze.py
@@ -1,22 +1,24 @@
 import collections
 import pandas as pd
+import numpy as np
 from datetime import datetime
 from utils import utils
+
 
 def analyze(df):
     result = {}
     try:
-        row_count = len(df)
+        print("start: {}".format(datetime.now()))
         for col in df.columns:
-            values = df[col].to_list()
-            c = collections.Counter([utils.judge_type(val) for val in values])
+            c = collections.Counter(df[col].map(type).to_list())
             result[col] = {
                 "most_common_type": c.most_common()[0][0].__name__,
                 "included_types": {val.__name__: c[val] for val in c.keys()},
-                "loss_rate": (float(c[type(None)]) / float(row_count)) * 100,
-                "all_items": len(values),
-                "unique_items": len(set(values))
+                "loss_rate": (float(df[col].isnull().sum()) / float(len(df[col]))) * 100,
+                "all_items": len(df[col]),
+                "unique_items": len(df[col].unique())
             }
+        print("finish: {}".format(datetime.now()))
     finally:
         return result
 
